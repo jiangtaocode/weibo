016a06b301655f779aaaeefbd4fa7852649cf886
jiangtao <97688510@qq.com>
2021-04-27 08:50:21 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '关于')
+@section('content')
+<h1>关于我们内容</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+<h1>帮助内容</h1>
+@stop
